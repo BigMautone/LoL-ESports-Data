2548b27484564c3f9024342fe6612715e5f49687
BigMautone <48888536+BigMautone@users.noreply.github.com>
2022-01-05 15:03:27 +0100

    Add files via upload

--- Query e Viste.sql
+++ Query e Viste.sql
@@ -6,14 +6,17 @@ AND (year(CURDATE()) - year(Data_di_nascita) < 20)
 AND (year(CURDATE()) - year(Data_inizio) <= 1);
 
 -- Vista giocatori ventenni --
-CREATE or REPLACE VIEW Giocatori_ventenni(Nickname,Nome,Cognome,Età) 
-AS SELECT Id, Nome, Cognome, year(CURDATE())-year(Data_di_nascita) from giocatore
-where year(CURDATE())-year(Data_di_nascita) < 20;
+CREATE or REPLACE VIEW Giocatori_ventenni(Nickname,Nome,Cognome,Età,Squadra, Data_inizio, Data_fine) 
+AS SELECT Id, Nome, Cognome, year(CURDATE())-year(Data_di_nascita), Squadra, Data_inizio, Data_fine
+FROM giocatore,contratto
+WHERE Id = Giocatore AND Scaduto = 0
+AND year(CURDATE())-year(Data_di_nascita) < 20;
+Select * from Giocatori_ventenni;
 
 -- Query op2 con vista--
-SELECT Nickname,Nome,Cognome,età, rateo(Uccisioni,Morti,Assist) as Rateo,Data_inizio as 'inizio nuovo contratto'
-FROM giocatori_ventenni,statistiche,contratto
-WHERE ((Nickname = statistiche.Giocatore and Nickname = contratto.Giocatore)
+SELECT Nickname,Nome,Cognome,età,Squadra,rateo(Uccisioni,Morti,Assist) as Rateo
+FROM giocatori_ventenni,statistiche
+WHERE ((Nickname = statistiche.Giocatore)
 and (rateo(Uccisioni,Morti,Assist) > 2.00)
 and year(CURDATE()) - year(Data_inizio) <= 1);
 
@@ -41,14 +44,14 @@ as (SELECT Id,Nome,Cognome,Data_nascita,Squadra,Stipendio
 	FROM staff
     Where Proprietario = 1
     Order by Squadra) ;
+SELECT * from Proprietario;
 
 -- Query 5 con vista "Proprietario"
-SELECT DISTINCT s.*
-from staff as s,proprietario as p
-WHERE p.Id != s.Id
-AND ((s.Stipendio > p.Stipendio and s.Squadra <> p.Squadra)
-OR s.Stipendio > any(SELECT Stipendio FROM contratto as c WHERE c.Squadra = s.Squadra))
-AND Proprietario = 0;
+SELECT s.*
+from staff as s
+WHERE ((s.Stipendio > all(SELECT Stipendio FROM Proprietario as p1 WHERE p1.Squadra <> s.Squadra))
+OR (s.Stipendio > any(SELECT Stipendio FROM contratto as c WHERE c.Squadra = s.Squadra and Scaduto = 0)))
+AND s.Proprietario = 0;
 
 -- op6
 SELECT vinto.Giocatore,Squadra_vincitrice, count(*) as trofei
@@ -136,19 +139,19 @@ FROM accordo , trofei_squadra as t
 where accordo.Squadra=t.Squadra 
 AND t.vittorie = (Select max(vittorie) from trofei_squadra);
             
--- op12 Trova la squadra che ha vinto meno tornei ma ha guadagnato di più
-
-SELECT et.Squadra_vincitrice as Squadra,sum(montepremi) as Vincita_totale, trofei
-FROM torneo,edizioni_torneo as et,(SELECT Squadra_vincitrice,torneo,count(*) as trofei FROM edizioni_torneo GROUP BY Squadra_vincitrice) as v
-WHERE nome = et.Torneo AND v.Squadra_vincitrice = et.Squadra_vincitrice AND nome = v.Torneo 
-GROUP BY Squadra
-HAVING (Vincita_totale >= all (SELECT sum(montepremi) FROM torneo,edizioni_torneo WHERE Nome = Torneo Group by squadra_vincitrice)
-OR trofei <= all (SELECT count(*) as trofei FROM edizioni_torneo GROUP BY Squadra_vincitrice))
-ORDER BY trofei asc, Vincita_totale desc;
+-- op12 Trova la squadra (o le squadre, se più  di 1) che ha vinto meno tornei ma ha guadagnato di più
+SELECT Squadra_vincitrice,sum(montepremi) as Vincite_totali, count(*) as Trofei
+FROM torneo,edizioni_torneo
+WHERE Nome = Torneo 
+GROUP BY squadra_vincitrice
+HAVING Vincite_totali = (SELECT max(vincite)
+			FROM (SELECT Squadra_vincitrice as Squadra,sum(montepremi) as vincite FROM torneo,edizioni_torneo WHERE Nome = Torneo Group by Squadra) as s1,
+            (SELECT Squadra_vincitrice as Squadra,count(*) as trofei FROM edizioni_torneo GROUP BY Squadra 
+            HAVING trofei <= ALL (SELECT count(*) FROM edizioni_torneo GROUP BY Squadra_vincitrice)) as s2
+            WHERE s1.Squadra = s2.Squadra);
 
 
 -- op13
-
 SELECT Giocatore,count(*) as Mondiali_vinti
 FROM vinto as v1,Torneo
 WHERE Torneo = Nome AND tipo = 'Mondiale'
@@ -167,32 +170,34 @@ WHERE Stipendio > all (SELECT Stipendio
                        WHERE (c1.Giocatore <> c2.Giocatore) and (c1.Squadra = c2.Squadra) and (Scaduto = 0));
                        
 -- Trova tutti i contratti dei giocatori che hanno giocato in + squadre
-SELECT c2.*
-FROM (SELECT *,count(*) as num_contratti
-		FROM contratto
-		GROUP BY Giocatore
-		ORDER BY num_contratti DESC) as c1,contratto as c2
-WHERE c2.Giocatore = c1.Giocatore AND num_contratti > 1;
+SELECT c1.Giocatore,Squadra,Data_inizio,Data_fine,Scaduto
+FROM(SELECT Giocatore,count(*)
+		FROM Contratto
+		Group By Giocatore
+		HAVING count(*) > 1) as c2,
+contratto as c1
+WHERE c1.Giocatore = c2.Giocatore;
 
--- op9 Trova i giocatori che hanno giocato contro una loro vecchia squadra NON FUNZIONA!!!
+-- op9 Trova i giocatori che hanno giocato contro una loro vecchia squadra 
+-- DA COMPLETARE
+-- Trova tutte le partite in cui alcuni giocatori hanno giocato contro la loro ex squadra
 SELECT p.*
-FROM partita as p
-WHERE (Squadra1 in (SELECT c2.Squadra
-				FROM (SELECT *,count(*) as num_contratti
-						FROM contratto
-						GROUP BY Giocatore
-						ORDER BY num_contratti DESC) as c1,contratto as c2
-				WHERE c2.Giocatore = c1.Giocatore AND num_contratti > 1)
-AND Squadra2 in (SELECT c2.Squadra
-				FROM (SELECT *,count(*) as num_contratti
-						FROM contratto
-						GROUP BY Giocatore
-						ORDER BY num_contratti DESC) as c1,contratto as c2
-				WHERE c2.Giocatore = c1.Giocatore AND num_contratti > 1));
-
-
-
-
+FROM partita as p,(SELECT c1.Giocatore,Squadra,Data_inizio,Data_fine,Scaduto
+					FROM(SELECT Giocatore,count(*)
+						FROM Contratto
+						Group By Giocatore
+						HAVING count(*) > 1) as c2,
+						contratto as c1
+					WHERE c1.Giocatore = c2.Giocatore and Scaduto = 0) as newG,
+					(SELECT c1.Giocatore,Squadra,Data_inizio,Data_fine,Scaduto
+					FROM(SELECT Giocatore,count(*)
+						FROM Contratto
+						Group By Giocatore
+						HAVING count(*) > 1) as c2,
+						contratto as c1
+					WHERE c1.Giocatore = c2.Giocatore and Scaduto = 1) as oldG
+WHERE ((newG.Squadra = Squadra1 and oldG.Squadra = Squadra2) OR (newG.Squadra = Squadra2 and oldG.Squadra = Squadra1))
+AND (newG.Data_inizio <= p.Data_partita)
 
 
 
